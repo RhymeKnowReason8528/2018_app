72028d06716b89dad8dac9475a9c80228bd92e77
Willilio <32961117+Willilio@users.noreply.github.com>
2017-11-03 10:11:45 -0400

    cleaned up teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -1,83 +1,29 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-//<<<<<<< HEAD
-//import com.qualcomm.robotcore.hardware.Servo;
-//import com.qualcomm.robotcore.util.ElapsedTime;
-//=======
+
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-//>>>>>>> 74177b342e3971f0edb3f3f2af92dfcf06861cf6
 
 /* 144 lines
  * Human-Controlled Operation program v 1.0 */
 
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="TeleOp", group="Linear Opmode")
 public class TeleOp extends LinearOpMode {
 
-    // Declare OpMode members.
-//<<<<<<< HEAD
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
-    private Servo gripperOneServo = null;
-    private Servo armServo = null;
 
-    final double GRIPPER_CLOSED = 0.72;
-    final double GRIPPER_RELIC_CLOSED = 0.87;
-    final double GRIPPER_OPEN = 0.59;
-
-    private double armPosition = armServo.getPosition();
-//=======
-
-    Robot robot = new Robot();
-//>>>>>>> 74177b342e3971f0edb3f3f2af92dfcf06861cf6
+    private Robot robot = new Robot();
 
     @Override
     public void runOpMode() {
+
+        // Declare OpMode members.
+        Servo gripperOneServo = null;
+        Servo armServo = null;
+
+        final double GRIPPER_CLOSED = 0.72;
+        final double GRIPPER_RELIC_CLOSED = 0.87;
+        final double GRIPPER_OPEN = 0.59;
 
         robot.init(hardwareMap);
 
@@ -87,19 +33,16 @@ public class TeleOp extends LinearOpMode {
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-//<<<<<<< HEAD
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+
         gripperOneServo.setPosition(GRIPPER_OPEN);
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightDrive.setDirection(DcMotor.Direction.FORWARD);
-//=======
-//>>>>>>> 74177b342e3971f0edb3f3f2af92dfcf06861cf6
+        robot.leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        robot.rightDrive.setDirection(DcMotor.Direction.FORWARD);
 
         // Wait for the game to start (driver presses PLAY)
+
         waitForStart();
         robot.runtime.reset();
 
@@ -136,10 +79,10 @@ public class TeleOp extends LinearOpMode {
                 armServo.setPosition(armServo.getPosition());
             }
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
+            /* Tank Mode uses one stick to control each wheel.
+            Right Trigger sets the position of the servo to one that will hold the block
+            Left Trigger sets the position of the servo to one that will hold the relic
+             - This requires no math, but it is hard to drive forward slowly and keep straight*/
 
             // Send calculated power to wheels
             robot.leftDrive.setPower(leftPower);
@@ -150,6 +93,7 @@ public class TeleOp extends LinearOpMode {
             telemetry.addData("Status", "Run Time: " + robot.runtime.toString());
             telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
             telemetry.addData("Servos", "gripper_one_closed (%)", (gripperOneServo.getPosition() == GRIPPER_CLOSED));
+            telemetry.addData("Arm", "arm_position (%)", (armServo.getPosition()));
             telemetry.update();
         }
     }
