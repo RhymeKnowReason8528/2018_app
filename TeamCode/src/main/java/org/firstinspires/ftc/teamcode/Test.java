a3906a364d7976d01067b6c2357cc55860522f40
Willilio <32961117+Willilio@users.noreply.github.com>
2018-01-19 13:06:51 -0500

    Added color sensor functionality

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by Will on 1/19/2018.
+ */
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Test")
+
+public class Test extends LinearOpMode{
+    private Robot robot = new Robot();
+
+    public void runOpMode() {
+
+        robot.init(hardwareMap, this);
+
+        robot.ledDisable();
+
+        int red;
+        int blue;
+        int green;
+
+        String bc = new String();
+
+        int bcNum;
+
+        robot.jewelT1Retract();
+//-------------------------------Wait for driver to press "play"-----------------------------------------------------
+        waitForStart();
+        robot.runtime.reset();
+//-------------------------------After driver presses "play"---------------------------------------------------------
+
+        robot.jewelT1Extend();
+
+        robot.ledEnable();
+
+        while (opModeIsActive()) {
+
+            //1 is red, 2 is blue
+
+            bc = "No Ball";
+
+            red = robot.getRed(1);
+            blue = robot.getBlue(1);
+            green = robot.getGreen(1);
+
+            bcNum = robot.getBallColor(red, blue);
+
+            if (bcNum == 1) {
+                bc = "Red";
+            } else if (bcNum == 2) {
+                bc = "Blue";
+            }
+
+            robot.jewelExtension1.setPosition(robot.jewelPosition);
+
+            telemetry.addData("Status: ", "Run Time: " + robot.runtime.toString());
+            telemetry.addData("Jewel Servo: ", "Position: " + robot.jewelExtension1.getPosition());
+            telemetry.addData("Color Sensor 1: ", "Red: " + red);
+            telemetry.addData("Color Sensor 1: ", "Blue: " + blue);
+            telemetry.addData("Color Sensor 1: ", "Green: " + green);
+            telemetry.addData("Ball Color: ", "red v blue: " + bc);
+            telemetry.update();
+
+        }
+    }
+}
